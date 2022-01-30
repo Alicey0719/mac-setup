e1366a010284dadd70b5a5a08ca4dc60e0f18151
Alicey <sirius@alicey.dev>
2022-01-30 23:40:35 +0900

    update rename

--- env/envinstall.sh
+++ env/envinstall.sh
@@ -3,4 +3,4 @@
 CURRENT=$(cd $(dirname $0);pwd)
 # echo $CURRENT/aaa
 cp $CURRENT/gitconfig.txt ~/.gitconfig
-cp $CURRENT/config.fish.txt ~/config/fish/config.fish
+cp $CURRENT/.config.fish.txt ~/config/fish/config.fish
