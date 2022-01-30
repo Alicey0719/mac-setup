48b578047f6734fae4d1a36e9b3ad52a730ef4da
Alicey <sirius@alicey.dev>
2022-01-31 08:39:34 +0900

    update envinstall.sh kubectl

--- env/envinstall.sh
+++ env/envinstall.sh
@@ -2,5 +2,13 @@
 
 CURRENT=$(cd $(dirname $0);pwd)
 # echo $CURRENT/aaa
+
+## git
 cp $CURRENT/gitconfig.txt ~/.gitconfig
+
+## fish
+mkdir -p ~/.config/fish/completions
 cp $CURRENT/config.fish.txt ~/.config/fish/config.fish
+cd ~/.config/fish/
+git clone https://github.com/evanlucas/fish-kubectl-completions.git
+cd -
