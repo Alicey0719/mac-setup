534eb05f22df83dc5b9df291b249a1bb71a27a74
Alicey <sirius@alicey.dev>
2022-01-31 08:51:57 +0900

    fix envinstall kubectl

--- env/envinstall.sh
+++ env/envinstall.sh
@@ -11,4 +11,5 @@ mkdir -p ~/.config/fish/completions
 cp $CURRENT/config.fish.txt ~/.config/fish/config.fish
 cd ~/.config/fish/
 git clone https://github.com/evanlucas/fish-kubectl-completions.git
+ln -s fish-kubectl-completions/completions/kubectl.fish completions/
 cd -
