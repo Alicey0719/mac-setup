592d1e0797248b839b28512dd8c3fef1269cfb13
Alicey <sirius@alicey.dev>
2022-02-20 22:17:17 +0900

    [update] node fish path

--- env/envinstall.sh
+++ env/envinstall.sh
@@ -6,12 +6,6 @@ CURRENT=$(cd $(dirname $0);pwd)
 ## git
 cp $CURRENT/gitconfig.txt ~/.gitconfig
 
-## node
-export PATH=$HOME/.nodebrew/current/bin:$PATH && \
-    source ~/.bashrc && \
-    nodebrew install-binary stable && \
-    nodebrew use stable
-
 ## fish
 mkdir -p ~/.config/fish/completions
 cp $CURRENT/config.fish.txt ~/.config/fish/config.fish
@@ -19,3 +13,10 @@ cd ~/.config/fish/
 git clone https://github.com/evanlucas/fish-kubectl-completions.git
 ln -s ~/.config/fish/fish-kubectl-completions/completions/kubectl.fish completions/
 cd -
+
+## node
+export PATH=$HOME/.nodebrew/current/bin:$PATH && \
+    source ~/.bashrc && \
+    set -x PATH $HOME/.nodebrew/current/bin $PATH && \
+    nodebrew install-binary stable && \
+    nodebrew use stable
